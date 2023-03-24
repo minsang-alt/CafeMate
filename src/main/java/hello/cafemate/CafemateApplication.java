dde751bdb93a1d514bec7446ac1b9bcad8f85a60
minsang-alt <mazinger97@naver.com>
2023-03-25 01:49:51 +0900

    setting

---
+++ src/main/java/hello/cafemate/CafemateApplication.java
@@ -0,0 +1,13 @@
+package hello.cafemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafemateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafemateApplication.class, args);
+	}
+
+}
