ea3d77b23f85d30e92ef16cc7181b69959a1938d
minsang-alt <mazinger97@naver.com>
2023-05-05 01:31:33 +0900

    회원가입구현완료DB연결필요

--- src/main/java/hello/cafemate/web/AuthController.java
+++ src/main/java/hello/cafemate/web/AuthController.java
@@ -1,11 +1,16 @@
 package hello.cafemate.web;
 
+import hello.cafemate.web.dto.auth.SignupDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AuthController {
+
+    private static final Logger log = LoggerFactory.getLogger(AuthController.class);
 
     @GetMapping("/auth/signin")
     public String signinForm(){
@@ -19,10 +24,11 @@ public class AuthController {
 
     //회원가입버튼누르면->/auth/signup->/auth/signin 리다이렉션
     @PostMapping("/auth/signup")
-    public String signup(){
+    public String signup(SignupDto signupDto){
 
-
-
+        //log.info(signupDto.toString());
+        //user객체에 userDto의 데이터들을 주입해야하고 서비스에 이 user오브젝트를 전달한다음 서비스는 각종일을 처리한다.
+        
 
         return "auth/signin";
     }
--- src/main/java/hello/cafemate/web/dto/auth/SignupDto.java
+++ src/main/java/hello/cafemate/web/dto/auth/SignupDto.java
@@ -5,8 +5,12 @@ import lombok.Data;
 @Data
 public class SignupDto {
 
-
-
+private String userId;
+private String password;
+private String email;
+private String fullName;
+private String nickName;
+private String userContact;
 
 
 
