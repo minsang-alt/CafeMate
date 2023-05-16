7394bb007f5fe419117075d233bdea9598cecdb9
calmmin <mj3242@naver.com>
2023-05-16 19:28:53 +0900

    feature : CustomerService 클래스 추가
    Service 계층 클래스들은 Controller 계층과 기본적으로 DTO를
    통하여 데이터를 교환하기에 Entity와 DTO, 양 타입간의 변환 메서드를
    구현해놓았다. 이외 기본적인 조회, 수정, 삭제를 수행하기 위한
    메서드와 삽입(회원 가입)을 실현하는 메서드를 구현하였다.

---
+++ src/main/java/hello/cafemate/service/CustomerService.java
@@ -0,0 +1,63 @@
+package hello.cafemate.service;
+
+import hello.cafemate.domain.Customer;
+import hello.cafemate.dto.simple_dto.CustomerDto;
+import hello.cafemate.dto.update_dto.CustomerUpdateDto;
+import hello.cafemate.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class CustomerService {
+    private final CustomerRepository customerRepository;
+
+    @Transactional
+    public Long join(CustomerDto dto){
+        Customer customer = dtoToEntity(dto);
+        validateDuplicateCustomer(customer.getCustomerId());
+        Customer saved = customerRepository.save(customer);
+        return saved.getId();
+    }
+
+    public CustomerDto findOne(CustomerDto dto){
+        return entityToDto(customerRepository.findByCustomerId(dto.getCustomerId()).get());
+    }
+
+    public List<CustomerDto> findAll(){
+        List<Customer> customers = customerRepository.findAll();
+        return customers.stream().map(this::entityToDto).collect(Collectors.toList());
+    }
+
+    public void updateOne(Long id, CustomerUpdateDto updateParam){
+        customerRepository.update(id, updateParam);
+    }
+
+    public void deleteOne(Long id){
+        customerRepository.deleteById(id);
+    }
+
+    private void validateDuplicateCustomer(String customerId){
+        Optional<Customer> result = customerRepository.findByCustomerId(customerId);
+
+        if(!result.isEmpty()){
+            throw new IllegalStateException("이미 가입된 고객입니다.");
+        }
+    }
+
+    private Customer dtoToEntity(CustomerDto dto){
+        return new Customer(dto.getCustomerId(), dto.getEMail(), dto.getPassword(),
+                dto.getName(), dto.getPhoneNumber(), dto.getAlias(), dto.getSavedPoint());
+    }
+
+    private CustomerDto entityToDto(Customer customer){
+        return new CustomerDto(customer.getCustomerId(), customer.getEMail(), customer.getPassword(),
+                customer.getName(), customer.getPhoneNumber(), customer.getAlias(), customer.getSavedPoint());
+    }
+}
